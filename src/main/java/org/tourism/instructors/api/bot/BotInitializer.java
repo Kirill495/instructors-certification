046f0f0fb7309c8d62887e666458461eb918347b
Kirill495 <k495@mail.ru>
2026-04-24 00:52:30 +0300

    fix empty tg token

--- src/main/java/org/tourism/instructors/api/bot/BotInitializer.java
+++ src/main/java/org/tourism/instructors/api/bot/BotInitializer.java
@@ -1,9 +1,8 @@
 package org.tourism.instructors.api.bot;
 
-import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.event.EventListener;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -22,7 +21,7 @@ public class BotInitializer {
 
     @EventListener(ApplicationReadyEvent.class)
     public void init() throws TelegramApiException {
-        if (StringUtil.isBlank(token)) {
+        if (token == null || token.isEmpty()) {
             return;
         }
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
